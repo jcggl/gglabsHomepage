3b2451d1d74f3ecee21c6d777d0ffd0fa995b891
LKH <mogies@naver.com>
2022-09-15 21:33:08 +0900

    feat: FooterPage apply tailwindcss

--- src/pages/FooterPage.tsx
+++ src/pages/FooterPage.tsx
@@ -4,27 +4,29 @@ import 'styles/FooterPage.scss'
 
 export default function FooterPage() {
   return (
-    <footer className='footer-container'>
-      <div className='footer-desc'>
-        <h3 className='desc-title extrabold italic'>
+    <footer className={String.raw`w-full max-w-[1200px] flex flex-col items-center mt-[clmap(120px,16vw,250px)] px-4 box-border`}>
+      <div className={String.raw`w-full flex flex-col items-start mb-[clamp(120px,15.6vw,316px)]`}>
+        <h3 className={String.raw`title`}>
           We are<br/>
           GoodGang Labs
         </h3>
-        <h5 className='desc-option bold'>
+        <h5 className={String.raw`text-white text-[clamp(16px,3.1vw,28px)] font-bold leading-[clamp(24px,3.1vw,36px)] mb-[clamp(10px,1.5vw,16px)]`}>
           Contact Us
         </h5>
-        <span className='desc-text light'>
+        <span className={String.raw`desc`}>
           {'Want to join our Eternal Beership?'}
           <br/>
           {'Shoot us an '}
-          <a className='desc-anchor bold' href='mailto:hello@goodganglabs.com'>
+          <a className={String.raw`text-[#a1a1a1] font-bold no-underline`}
+            href='mailto:hello@goodganglabs.com'>
             hello@goodganglabs.com
           </a>
         </span>
       </div>
-      <div className='footer-info'>
-        <img className='footer-logo' src={secondLogo} alt="logo" />
-        <p className='footer-copyright'>
+      <div className={String.raw`w-full self-start pb-[min(5.86vw,78px)]`}>
+        <img className={String.raw`w-[min(33%,150px)] aspect-[3/1] mb-[clamp(12px,1.56vw,15px)]`}
+          src={secondLogo} alt="logo" />
+        <p className={String.raw`text-[#a1a1a1] text-[clamp(10px,1.5vw,12px)] leading-[150%]`}>
           Ⓒ2022 GOOD GANG LABS PTE. LTD.
         </p>
       </div>
