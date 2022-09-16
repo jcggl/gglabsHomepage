4fdf62c0702af70180f7aa10c06f763c15d52a38
LKH <mogies@naver.com>
2022-09-16 09:30:12 +0900

    refact: remove console.log

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function ItemCard({ children, classes }: any) {
-  console.log(classes)
   return (
     <div className={String.raw`flex flex-col justify-center items-start rounded-[8px] p-[clamp(16px,2.34vw,40px)] relative break-words box-border ` + classes}>
       {children}
